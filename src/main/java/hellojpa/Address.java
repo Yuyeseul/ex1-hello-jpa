7097a2c702b99b0649e08cab86bb110f3c032caf
Yuyeseul <ryuys0308@naver.com>
2025-05-27 18:20:18 +0900

    값 타입의 비교

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 @Embeddable
 public class Address {
 
@@ -28,5 +30,18 @@ public class Address {
 
     public String getZipcode() {
         return zipcode;
+    }
+
+    // equals 재정의
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
     }
 }
--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -4,16 +4,15 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
-//        Integer c = new Integer(10);
-//        Integer d = c;
-//        d.setValue(20) // c, d => 20
-
         int a = 10;
-        int b = a;
+        int b = 10;
 
-        a = 20;
+        System.out.println("a == b : " + (a == b));
 
-        System.out.println("a = " + a); // 10
-        System.out.println("b = " + b); // 20
+        Address address1 = new Address("city", "street", "zipcode");
+        Address address2 = new Address("city", "street", "zipcode");
+
+        System.out.println("address1 == address2 : " + (address1 == address2)); // 동일성 비교 : false 참조값 다름
+        System.out.println("address1 equals address2 : " + (address1.equals(address2))); // 동등성 비교 : false equals의 기본이 == 비교 -> 재정의
     }
 }
